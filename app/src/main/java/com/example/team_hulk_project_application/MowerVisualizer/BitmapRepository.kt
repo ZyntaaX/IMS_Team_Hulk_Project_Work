2ed0b685224dd5e7a11762bd54ca23f91af00992
Rasmus Svanberg <svra18cy@student.ju.se>
2021-04-07 13:53:30 +0200

    Prepared Mower Visualizer

---
+++ app/src/main/java/com/example/team_hulk_project_application/MowerVisualizer/BitmapRepository.kt
@@ -0,0 +1,7 @@
+package com.example.team_hulk_project_application.MowerVisualizer
+
+val bitmapRepository = BitmapRepository()
+
+class BitmapRepository {
+
+}
---
+++ app/src/main/java/com/example/team_hulk_project_application/MowerVisualizer/ImageLayer.kt
@@ -0,0 +1,5 @@
+package com.example.team_hulk_project_application.MowerVisualizer
+
+/*data class ImageLayer () {
+
+}*/
