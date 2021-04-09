f8329627e2804dd0db9facc7092ce0b7d48cb583
daax <daax18mk@student.ju.se>
2021-04-09 17:41:50 +0200

    Started on the UI for connecting to the Mower

--- app/src/main/java/com/example/team_hulk_project_application/MainActivity.kt
+++ app/src/main/java/com/example/team_hulk_project_application/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.team_hulk_project_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import com.example.team_hulk_project_application.Firestore.db
 import com.example.team_hulk_project_application.Firestore.firestoreManager
 
@@ -11,5 +15,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         firestoreManager.TestFirestore("HEJHEJ");
+        connectToMower()
+    }
+
+    private fun connectToMower(){
+        val connectButton = findViewById<Button>(R.id.connectButton)
+        val wifiIcon = findViewById<ImageView>(R.id.wifiEnabledIcon)
+
+        var connectionStatus = 0
+
+        connectButton.setOnClickListener{
+            if(connectionStatus == 0){
+                wifiIcon.visibility = View.VISIBLE
+                connectButton.text = "Disconnect from Mower"
+                connectionStatus = 1
+                Toast.makeText(this, "You are connected to the Mower", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                wifiIcon.visibility = View.INVISIBLE
+                connectButton.text = "Connect to Mower"
+                connectionStatus = 0
+                Toast.makeText(this, "You are disconnected from the Mower", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
